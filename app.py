7a4d39f9d14f2e5cd8956633d8002ac763836494
Md Ibrahim Hossain <86115678+Mdibrahim789@users.noreply.github.com>
2026-06-13 11:43:01 +0600

    fix app startup

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+from fastapi import FastAPI, Request, Response
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+return {"status": "running"}
+
 @app.post("/pathao/webhook")
 async def webhook(request: Request):
 
-    data = await request.json()
-    print("PATHAO WEBHOOK:", data)
+```
+data = await request.json()
+print("PATHAO WEBHOOK:", data)
 
-    return Response(
-        content="OK",
-        status_code=202,
-        headers={
-            "X-Pathao-Merchant-Webhook-Integration-Secret":
-            "f3992ecc-59da-4cbe-a049-a13da2018d51"
-        }
-    )
+return Response(
+    content="OK",
+    status_code=202,
+    headers={
+        "X-Pathao-Merchant-Webhook-Integration-Secret":
+        "f3992ecc-59da-4cbe-a049-a13da2018d51"
+    }
+)
+```
